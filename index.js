1a7899f486142bcd8920b3f95be22caa12cc692d
angelspacenter111 <angel.spacenter111@gmail.com>
2026-05-02 20:53:16 +0530

    test

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.use("/", webRoutes);
 
 // Server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on https://cahtapp.netlify.app:${PORT}`);
 });
